f5d7c9d96ab644028480b7cd5c6ecbb27d2d72ab
cVitorAndrade <vitorandrade1804@gmail.com>
2024-02-05 19:32:31 -0300

    initial commit

---
+++ src/pages/Portfolio/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Portfolio () {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+export default {
+    COLORS: {
+        WHITE_900: "#FFFFFF",
+        BLACK_900: "#000000",
+        GREEN_900: "#346259",
+    }
+}
